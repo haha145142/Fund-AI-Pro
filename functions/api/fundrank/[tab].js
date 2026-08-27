58b024a6c61860300cebd2a5c90174b6e9ae658b
haha145142 <276353662@qq.com>
2026-08-28 02:55:29 +0800

    Update [tab].js

--- functions/api/fundrank/[tab].js
+++ functions/api/fundrank/[tab].js
@@ -1,1 +1,11 @@
-
+export function onRequest({ params }) {
+  const tab = params.tab || "";
+  return new Response(JSON.stringify({
+    ok: true,
+    route: "fundrank",
+    tab,
+    params
+  }), {
+    headers: { "content-type": "application/json;charset=utf-8" }
+  });
+}
