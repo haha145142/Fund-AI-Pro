7e3bf36324a9e220363faef39a9570fbbbf87dce
haha145142 <276353662@qq.com>
2026-08-28 02:38:55 +0800

    Delete functions/api/fundrank/Code_20260828023403.js

--- functions/api/fundrank/Code_20260828023403.js
+++
@@ -1,6 +0,0 @@
-// functions/api/health.js
-export function onRequest() {
-  return new Response(JSON.stringify({ ok: true, ts: Date.now() }), {
-    headers: { 'content-type': 'application/json' }
-  });
-}
